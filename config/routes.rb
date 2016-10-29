006e7f8b944e62fc63ccd2c08df2eca235e0a082
Aaron Goldsmith <aarongoldsmith@msn.com>
2016-10-28 21:49:36 -0700

    added user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
+
+root 'users#index'
+
+get 'users' => 'users#index'
+post 'users' => 'users#create'
+get 'users/new' => 'users#new', as: :new_user
+get 'users/:id' => 'users#show', as: :user
+get 'users/:id/edit' => 'users#edit', as: :edit_user
+patch 'users/:id' => 'users#update'
+delete 'users/:id' => 'users#destroy'
+
+get 'login' => 'sessions#new'
+post 'login' => 'sessions#create'
+delete 'logout' => 'sessions#destroy'
+
+
+
+
+
+
+
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
