7d6eb5124099fbf150b122c1ec01db4524500aa6
Aaron Goldsmith <aarongoldsmith@msn.com>
2016-10-28 21:33:29 -0700

    replaced user address migration

--- db/migrate/20161029040819_add_addresses_to_user.rb
+++
@@ -1,4 +0,0 @@
-class AddAddressesToUser < ActiveRecord::Migration[5.0]
-  def change
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161029040819) do
+ActiveRecord::Schema.define(version: 20161029043208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 20161029040819) do
     t.float    "total_fees"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_addresses_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,4 +37,5 @@ ActiveRecord::Schema.define(version: 20161029040819) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "addresses", "users"
 end
